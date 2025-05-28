ecf88c0e73cbb04c28a78f0c1964c14c037c492a
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2025-05-28 15:59:08 -0300

    corrigindo end-point login

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/auth/login', {
+      const response = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
