214a03e747aa18ab906444f15835ed1053f56def
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2026-01-22 01:51:51 -0300

    ajustando /api do login

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://52.67.252.192/auth/login', {
+      const response = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
