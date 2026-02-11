90721250bf7efaeb9a07e37620ee9de662fe80fb
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2026-02-11 15:55:27 -0300

    feat: mudança no form de visita e adicionando rota para buscar endereço5

--- front-end/src/components/NovaVisita.js
+++ front-end/src/components/NovaVisita.js
@@ -222,7 +222,7 @@ export default function VisitaForm() {
         },
       };
 
-      const resp = await fetch(API_BASE, {
+      const resp = await fetch(`${API_BASE}/visitas`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
