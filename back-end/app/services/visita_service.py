8cd5771f874758d9b8f4cea9593bfec1d7b7fc59
gustavohnrq <gustavohenriquer598@gmail.com>
2026-03-12 13:08:17 -0300

    fix: apontando paraa panilha correta

--- back-end/app/services/visita_service.py
+++ back-end/app/services/visita_service.py
@@ -34,7 +34,7 @@ from google.auth.exceptions import RefreshError
 # CONFIG
 # =========================
 # Use o mesmo ID lido pelo Apps Script
-SPREADSHEET_ID = "1isFLYaYbaKEZrsPDbU1Bc0cswyFUgTElcQf2CNXx0Hc"
+SPREADSHEET_ID = "1we1qAVRBqAWaXmOfnLnFJzCi8WPt-ZEhxKb0Ab9DiQU"
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # app/
 OAUTH_CLIENT_FILE = os.path.join(BASE_DIR, "utils", "asserts", "oauth.json")
