fd4fadd78edc81e3d8c65728e433a92b607246bb
Inteligencia61 <inteligencia.61imoveis@gmail.com>
2026-03-30 17:55:27 -0300

    fix: correcao lista user2

--- back-end/app/services/rela_gerentes_service.py
+++ back-end/app/services/rela_gerentes_service.py
@@ -16,7 +16,7 @@ load_dotenv()
 API_BASE = (os.getenv("API_BASE") or "").rstrip("/")
 
 def _buscar_ids_corretores_ativos() -> set[str]:
-    usuarios_ativos = retornar_lista(ativo=True).get("lista", [])
+    usuarios_ativos = retornar_lista(ativo=True, page=1, per_page=100000)
 
     return {
         _safe_str(usuario.get("id_usuarios"))
