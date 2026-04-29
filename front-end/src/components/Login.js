373d2c999a52c36e5fd634120c4b8978f8c25806
Inteligencia61 <inteligencia.61imoveis@gmail.com>
2026-04-29 13:02:12 -0300

    fix: melhorando back de lançar visita

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -17,8 +17,8 @@ function Login() {
     setLoading(true);
 
     try {
-      //const response = await fetch('/api/auth/login', {
-      const response = await fetch('http://localhost:5000/auth/login', {  
+      const response = await fetch('/api/auth/login', {
+      //const response = await fetch('http://localhost:5000/auth/login', {  
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
