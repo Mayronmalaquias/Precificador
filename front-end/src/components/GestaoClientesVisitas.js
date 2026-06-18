568214b08b04cf03e48e16d0520803d7163da5be
Mayronmalaquias <mayronmalaquias1607@gmail.com>
2026-06-18 17:20:08 -0300

    feat: adicionando + 3 em filtros de visita

--- front-end/src/components/GestaoClientesVisitas.js
+++ front-end/src/components/GestaoClientesVisitas.js
@@ -663,6 +663,7 @@ function GestaoClientesVisitas() {
           >
             <option value="0">Todas</option>
             <option value="1">1+</option>
+            <option value="3">3+</option>
             <option value="5">5+</option>
             <option value="10">10+</option>
             <option value="15">15+</option>
